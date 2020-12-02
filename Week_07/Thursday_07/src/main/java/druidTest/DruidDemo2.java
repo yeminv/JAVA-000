8b04881b9784d0eec2bf825397353c32efc7dbdb
min-ye-tfs <min.ye@thermofisher.com>
2020-12-02 22:15:50 +0800

    2.（必做）按自己设计的表结构，插入 100 万订单模拟数据，测试不同方式的插入效率

--- Week_07/Thursday_07/src/main/java/druidTest/DruidDemo2.java
+++ Week_07/Thursday_07/src/main/java/druidTest/DruidDemo2.java
@@ -16,12 +16,14 @@ public class DruidDemo2 {
         try {
             //1.获取连接
             conn = JDBCUtils.getConnection();
-            //2.定义sql
-            String sql = "INSERT INTO `order` (order_no, order_sn, member_id, supplier_id, supplier_name, order_status, after_status, product_count, product_amount_total, order_amount_total, logistics_fee, address_id, pay_channel, out_trade_no, escrow_trade_no, pay_time, delivery_time, order_settlement_status, order_settlement_time, is_package, is_integral, created_at, updated_at, deleted_at) VALUES('1', '1', 1, 1, 'testSupplier', 0, 0, 1, 100.0000, 100.0000, 0.0000, 225400, 0, '1', '1', 0, 0, 0, 0, '0', '0', NULL, NULL, NULL)";
-            //3.获取pstmt对象
-            pstmt = conn.prepareStatement(sql);
-            //4.执行sql
-            pstmt.executeUpdate();
+            for (int i = 0; i < 1000000; i++) {
+                //2.定义sql
+                String sql = "INSERT INTO `order` (order_no, order_sn, member_id, supplier_id, supplier_name, order_status, after_status, product_count, product_amount_total, order_amount_total, logistics_fee, address_id, pay_channel, out_trade_no, escrow_trade_no, pay_time, delivery_time, order_settlement_status, order_settlement_time, is_package, is_integral, created_at, updated_at, deleted_at) VALUES('1', '1', 1, 1, 'testSupplier', 0, 0, 1, 100.0000, 100.0000, 0.0000, 225400, 0, '1', '1', 0, 0, 0, 0, '0', '0', NULL, NULL, NULL)";
+                //3.获取pstmt对象
+                pstmt = conn.prepareStatement(sql);
+                //4.执行sql
+                pstmt.executeUpdate();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
